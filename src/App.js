dfb2d36e761b3a5837b1539ab5f3da2dc41e7a78
kindOfCurly <marie.sautmann@gmx.de>
2021-01-24 15:39:03 +0100

    feat/#14: added feedback screen design and route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import "./App.css";
+import Feedbackscreen from "./components/pages/Feedbackscreen";
 import Homescreen from "./components/pages/Homescreen";
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Homescreen} />
+          <Route path="/feedback" component={Feedbackscreen} />
         </Switch>
       </div>
     </Router>
---
+++ src/components/pages/Feedbackscreen.js
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+    Button,
+    withStyles
+} from "@material-ui/core/";
+import TopAppBar from "../pageComponents/TopAppBar";
+
+// todo: topappbar should not always display logo
+
+const styles = (theme) => ({
+    container: {
+        marginStart: "16px",
+        marginEnd: "16px",
+    },
+
+    text: {
+        color: "#2E303C",
+        textAlign: "start",
+    },
+
+    textarea: {
+        border: "solid 1px #A7525E",
+        boxSizing: "border-box",
+        width: "100%",
+    },
+
+    button: {
+        color: "#00817B",
+        float: "right",
+    },
+});
+
+class Feedbackscreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 'Bitte geben Sie hier Ihr Feedback ein.' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit() {
+        console.log("Feedback was submitted: " + this.state.value);
+        // todo: send feedback via email
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className="Feedbackscreen">
+                <TopAppBar />
+                <div className={classes.container}>
+                    <p className={classes.text}>Sie können Ihr Feedback per eMail an <a href="mailto:abc@example.com">xy@ur.de</a> oder direkt hier in der App senden.</p>
+                    <form>
+                        <textarea className={classes.textarea} value={this.state.value} onChange={this.handleChange} rows="10" />
+                    </form>
+                    <Button className={classes.button} onClick={this.handleSubmit}>Senden</Button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(Feedbackscreen);
