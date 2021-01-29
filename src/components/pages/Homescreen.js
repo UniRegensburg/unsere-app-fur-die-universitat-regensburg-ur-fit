370601629397e6d383fc6f64e45641160733851f
kindOfCurly <marie.sautmann@gmx.de>
2021-01-29 15:56:51 +0100

    feat/#17: start screen
    Removed bottom navgation bar as it is done in #34

--- src/components/pages/Homescreen.js
+++ src/components/pages/Homescreen.js
@@ -2,12 +2,8 @@ import React from "react";
 import { withStyles } from "@material-ui/core/";
 import TopAppBar from "../pageComponents/TopAppBar";
 import ContentCard from "../pageComponents/ContentCard";
-import BottomNavigation from "@material-ui/core/BottomNavigation";
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import { Link } from "react-router-dom";
 
 import * as TestContent from "../../constants/testContent.js";
-import * as ConstantPages from "../../constants/constants.js";
 
 const styles = (theme) => ({
   container: {
@@ -49,64 +45,9 @@ class Homescreen extends React.Component {
           <ContentCard data-testid="content-item" data={testdata.test3} />
           <ContentCard data-testid="content-item" data={testdata.test4} />
         </div>
-
-        <SimpleBottomNavigation
-          class={classes.bottomNavigation}
-          icon={classes.icon}
-        />
       </div>
     );
   }
 }
 
 export default withStyles(styles)(Homescreen);
-
-class SimpleBottomNavigation extends React.Component {
-  render() {
-    let relaxation = ConstantPages.pages.relaxation,
-      fitness = ConstantPages.pages.fitness,
-      wellbeing = ConstantPages.pages.wellbeing,
-      nutrition = ConstantPages.pages.nutrition;
-
-    return (
-      <BottomNavigation
-        data-testid="bottomnavigation"
-        showLabels
-        className={this.props.class}
-      >
-        <BottomNavigationAction
-          data-testid="navigation-relaxation"
-          className={this.props.icon}
-          label={relaxation.title}
-          icon={relaxation.icon}
-          component={Link}
-          to={relaxation.value}
-        />
-        <BottomNavigationAction
-          data-testid="navigation-fitness"
-          className={this.props.icon}
-          label={fitness.title}
-          icon={fitness.icon}
-          component={Link}
-          to={fitness.value}
-        />
-        <BottomNavigationAction
-          data-testid="navigation-wellbeing"
-          className={this.props.icon}
-          label={wellbeing.title}
-          icon={wellbeing.icon}
-          component={Link}
-          to={wellbeing.value}
-        />
-        <BottomNavigationAction
-          data-testid="navigation-nutrition"
-          className={this.props.icon}
-          label={nutrition.title}
-          icon={nutrition.icon}
-          component={Link}
-          to={nutrition.value}
-        />
-      </BottomNavigation>
-    );
-  }
-}
--- src/tests/pageComponents/Homescreen.test.js
+++ src/tests/pageComponents/Homescreen.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import Homescreen from "../../components/pages/Homescreen.js";
@@ -13,73 +13,7 @@ it("check if content is displayed", () => {
   );
   const appbar = getByTestId("appbar");
   const title = getByTestId("title");
-  const navigation = getByTestId("bottomnavigation");
-  const relaxationButton = getByTestId("navigation-relaxation");
-  const fitnessButton = getByTestId("navigation-fitness");
-  const wellbeingButton = getByTestId("navigation-wellbeing");
-  const nutritionButton = getByTestId("navigation-nutrition");
 
   expect(appbar).toBeInTheDocument();
   expect(title).toBeInTheDocument();
-  expect(navigation).toBeInTheDocument();
-  expect(relaxationButton).toBeInTheDocument();
-  expect(fitnessButton).toBeInTheDocument();
-  expect(wellbeingButton).toBeInTheDocument();
-  expect(nutritionButton).toBeInTheDocument();
-});
-
-it("check if url changes on relaxation click", () => {
-  global.window = { location: { pathname: null } };
-  const { getByTestId } = render(
-    <Router>
-      <Homescreen />
-    </Router>
-  );
-  const relaxationButton = getByTestId("navigation-relaxation");
-
-  fireEvent.click(relaxationButton);
-
-  expect(global.window.location.pathname).toEqual("/relaxation");
-});
-
-it("check if url changes on fitness click", () => {
-  global.window = { location: { pathname: null } };
-  const { getByTestId } = render(
-    <Router>
-      <Homescreen />
-    </Router>
-  );
-  const fitnessButton = getByTestId("navigation-fitness");
-
-  fireEvent.click(fitnessButton);
-
-  expect(global.window.location.pathname).toEqual("/fitness");
-});
-
-it("check if url changes on wellbeing click", () => {
-  global.window = { location: { pathname: null } };
-  const { getByTestId } = render(
-    <Router>
-      <Homescreen />
-    </Router>
-  );
-  const wellbeingButton = getByTestId("navigation-wellbeing");
-
-  fireEvent.click(wellbeingButton);
-
-  expect(global.window.location.pathname).toEqual("/wellbeing");
-});
-
-it("check if url changes on nutrition click", () => {
-  global.window = { location: { pathname: null } };
-  const { getByTestId } = render(
-    <Router>
-      <Homescreen />
-    </Router>
-  );
-  const nutritionButton = getByTestId("navigation-nutrition");
-
-  fireEvent.click(nutritionButton);
-
-  expect(global.window.location.pathname).toEqual("/nutrition");
 });
