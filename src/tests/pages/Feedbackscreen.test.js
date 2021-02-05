d1bf4bc7317247212436e4d3bfc07ac2841687eb
kindOfCurly <marie.sautmann@gmx.de>
2021-02-05 19:42:53 +0100

    feat/#45: add home icon
    Fixed tests to use a Router (because of adding a Link to the AppBar)

--- src/tests/pages/Feedbackscreen.test.js
+++ src/tests/pages/Feedbackscreen.test.js
@@ -1,24 +1,33 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import Feedbackscreen from "../../components/pages/Feedbackscreen";
 
 it("check if screen content displays", () => {
-    const { getByTestId } = render(<Feedbackscreen />);
-    const appbar = getByTestId("appbar");
-    const text = getByTestId("feedback-text");
-    const textarea = getByTestId("feedback-textarea");
-    const button = getByTestId("feedback-button");
-    
-    expect(appbar).toBeInTheDocument();
-    expect(text).toBeInTheDocument();
-    expect(textarea).toBeInTheDocument();
-    expect(button).toBeInTheDocument();
-  });
+  const { getByTestId } = render(
+    <Router>
+      <Feedbackscreen />
+    </Router>
+  );
+  const appbar = getByTestId("appbar");
+  const text = getByTestId("feedback-text");
+  const textarea = getByTestId("feedback-textarea");
+  const button = getByTestId("feedback-button");
+
+  expect(appbar).toBeInTheDocument();
+  expect(text).toBeInTheDocument();
+  expect(textarea).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
+});
 
 it("check if textarea displays text", () => {
-  const { getByTestId } = render(<Feedbackscreen />);
+  const { getByTestId } = render(
+    <Router>
+      <Feedbackscreen />
+    </Router>
+  );
   const textarea = getByTestId("feedback-textarea");
 
   fireEvent.change(textarea, { target: { value: "feedback" } });
