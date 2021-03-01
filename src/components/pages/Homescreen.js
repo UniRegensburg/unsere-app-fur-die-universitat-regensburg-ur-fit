febaefd4fdbd2c0c87077b191b6bb456dec77c16
kindOfCurly <marie.sautmann@gmx.de>
2021-03-01 11:10:13 +0100

    feat/#75: added welcome + info message
    - on start screen a welcome message is used to greet the user
    - if list of favorites is empty a info message is shown

--- src/components/pages/Homescreen.js
+++ src/components/pages/Homescreen.js
@@ -2,9 +2,12 @@ import React from "react";
 import { withStyles } from "@material-ui/core/";
 import TopAppBar from "../pageComponents/TopAppBar";
 import ContentCard from "../pageComponents/ContentCard";
+import InfoMessageCard from "../pageComponents/InfoMessageCard";
 
 import * as Constants from "../../constants/constants.js";
 import BottomNavigationBar from "../pageComponents/BottomNavigationBar";
+
+const username = "Viktor"; // get from db
 
 const styles = (theme) => ({
   container: {
@@ -17,24 +20,52 @@ const styles = (theme) => ({
     color: "#2E303C",
     textAlign: "start",
   },
+
+  card: {
+    border: "solid 2px #00817B",
+    marginBottom: "16px",
+  },
+
+  cardContent: {
+    "&:last-child": {
+      paddingBottom: "16px",
+    },
+  },
 });
 
 class Homescreen extends React.Component {
   render() {
-    // testdata ist not tested for favorite=true (because it is dummy data)
+    // testdata is not tested for favorite=true (because it is dummy data)
     let data = Constants.pages.favorites;
     const { classes } = this.props;
+
+    let content;
+    if (Object.getOwnPropertyNames(data).length !== 0) {
+      // if we have unique keys use: Object.keys(data).length
+      // show favorites
+      content = (
+        <div>
+          <h3 data-testid="title" className={classes.text}>
+            Deine Lieblingsübungen
+          </h3>
+          {data.content.map((item) => (
+            <ContentCard data-testid="content-item" data={item} />
+          ))}
+        </div>
+      );
+    } else {
+      // no favorites, show info
+      content = <InfoMessageCard />;
+    }
 
     return (
       <div className="Homescreen">
         <TopAppBar data-testid="appbar" title="URfit" />
         <div className={classes.container}>
-          <h3 data-testid="title" className={classes.text}>
-            Meine Lieblingsübungen
-          </h3>
-          {data.content.map((item, index) => (
-            <ContentCard data-testid="content-item" data={item} />
-          ))}
+          <h4 className={classes.text}>
+            Hallo {username}! Willkommen bei URfit.
+          </h4>
+          {content}
         </div>
         <BottomNavigationBar />
       </div>
