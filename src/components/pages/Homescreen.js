c5a6994ec9ef13358c758a72911968f18ba46623
orembold <62057626+orembold@users.noreply.github.com>
2021-03-03 19:47:12 +0100

    feat/#99_fixed_merge_conflicts

--- src/components/pages/Homescreen.js
+++ src/components/pages/Homescreen.js
@@ -7,10 +7,9 @@ import InfoMessageCard from "../pageComponents/InfoMessageCard";
 
 import BottomNavigationBar from "../pageComponents/BottomNavigationBar";
 
-const useStyles = makeStyles((theme) => ({
 const username = "Viktor"; // get from db
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   container: {
     marginStart: "16px",
     marginEnd: "16px",
@@ -20,6 +19,17 @@ const styles = (theme) => ({
   text: {
     color: "#2E303C",
     textAlign: "start",
+  },
+
+  card: {
+    border: "solid 2px #00817B",
+    marginBottom: "16px",
+  },
+
+  cardContent: {
+    "&:last-child": {
+      paddingBottom: "16px",
+    },
   },
 }));
 
@@ -34,74 +44,33 @@ export default function Homescreen() {
         next: (querySnapshot) => {
           var favorites = [];
           let userFavorites = querySnapshot.data().favorites;
-          userFavorites.forEach((favorite, index, favs) => {
-            getContentById(favorite.id).then((content) => {
-              favorites.push(content.data());
-              if (Object.is(favs.length - 1, index)) {
-                setFavorites(
-                  <div>
-                    {favorites.map((item, index) => {
-                      item.favorite = true;
-                      return (
-                        <ContentCard data-testid="content-item" data={item} />
-                      );
-                    })}
-                  </div>
-                );
-              }
-            });
-          });
+          userFavorites.length === 0
+            ? setFavorites(<InfoMessageCard />)
+            : userFavorites.forEach((favorite, index, favs) => {
+                getContentById(favorite.id).then((content) => {
+                  favorites.push(content.data());
+                  if (Object.is(favs.length - 1, index)) {
+                    setFavorites(
+                      <div>
+                        <h3 data-testid="title" className={classes.text}>
+                          Deine Lieblingsübungen
+                        </h3>
+                        {favorites.map((item, index) => {
+                          item.favorite = true;
+                          return (
+                            <ContentCard
+                              data-testid="content-item"
+                              data={item}
+                            />
+                          );
+                        })}
+                      </div>
+                    );
+                  }
+                });
+              });
         },
       }
-
-  card: {
-    border: "solid 2px #00817B",
-    marginBottom: "16px",
-  },
-
-  cardContent: {
-    "&:last-child": {
-      paddingBottom: "16px",
-    },
-  },
-});
-
-class Homescreen extends React.Component {
-  render() {
-    // testdata is not tested for favorite=true (because it is dummy data)
-    let data = Constants.pages.favorites;
-    const { classes } = this.props;
-
-    let content;
-    if (Object.getOwnPropertyNames(data).length !== 0) {
-      // if we have unique keys use: Object.keys(data).length
-      // show favorites
-      content = (
-        <div>
-          <h3 data-testid="title" className={classes.text}>
-            Deine Lieblingsübungen
-          </h3>
-          {data.content.map((item) => (
-            <ContentCard data-testid="content-item" data={item} />
-          ))}
-        </div>
-      );
-    } else {
-      // no favorites, show info
-      content = <InfoMessageCard />;
-    }
-
-    return (
-      <div className="Homescreen">
-        <TopAppBar data-testid="appbar" title="URfit" />
-        <div className={classes.container}>
-          <h4 className={classes.text}>
-            Hallo {username}! Willkommen bei URfit.
-          </h4>
-          {content}
-        </div>
-        <BottomNavigationBar />
-      </div>
     );
     return unsubscribe;
   }
@@ -114,9 +83,9 @@ class Homescreen extends React.Component {
     <div className="Homescreen">
       <TopAppBar data-testid="appbar" title="URfit" />
       <div className={classes.container}>
-        <h3 data-testid="title" className={classes.text}>
-          Meine Lieblingsübungen
-        </h3>
+        <h4 className={classes.text}>
+          Hallo {username}! Willkommen bei URfit.
+        </h4>
         {spinner}
       </div>
       <BottomNavigationBar />
