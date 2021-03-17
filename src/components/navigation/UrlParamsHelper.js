cb80f15e2e2780d700673e4a2202432f9116f404
orembold <62057626+orembold@users.noreply.github.com>
2021-03-17 12:32:45 +0100

    feat/#122_fixed_loading_bug
    Structure was not loaded, when entering the app though a link, that is not the homepage

--- src/components/navigation/UrlParamsHelper.js
+++ src/components/navigation/UrlParamsHelper.js
@@ -1,7 +1,5 @@
 import { useContext } from "react";
 
-import * as Constants from "../../constants/constants";
-
 import Contentlistscreen from "../pages/Contentlistscreen";
 import Detailscreen from "../pages/Detailscreen";
 import Mensascreen from "../pages/Mensascreen";
@@ -15,11 +13,7 @@ export const Category = ({ match }) => {
   );
   return (
     <>
-      <CategoryList
-        title={category.title}
-        categories={category.subcategories}
-        match={match}
-      />
+      <CategoryList category={category} match={match} />
     </>
   );
 };
